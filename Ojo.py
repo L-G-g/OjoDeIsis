0aee946f90e20fab347ab02b3b5685a5612995ec
L-G-g <67565074+L-G-g@users.noreply.github.com>
2022-12-27 21:41:48 -0300

    Se descomprime el ultimo archivo .zip

--- Ojo.py
+++ Ojo.py
@@ -2,19 +2,23 @@ from selenium import webdriver
 import time
 import zipfile
 import os
+import glob
 
+#Guarda el path del directorio de trabajo
+patcito = os.path.realpath(os.path.dirname(__file__))
 
-
+#Configuracion del WebDriver
 options = webdriver.ChromeOptions()
-prefs = {"download.default_directory" : os.path.realpath(os.path.dirname(__file__))}
-
+prefs = {"download.default_directory" : patcito}
 options.add_experimental_option("prefs",prefs)
-
 driver = webdriver.Chrome(chrome_options=options)
-
+#Webdriver va al link de descarga
 driver.get('https://ssl.smn.gob.ar/dpd/zipopendata.php?dato=tiepre')
 
-time.sleep(5)
+#Consigue el path del archivo recien descargado
+list_of_files = glob.glob(patcito+"\*.zip")
+latest_file = max(list_of_files, key=os.path.getctime)
 
-#with zipfile.ZipFile(__file__, 'r') as zip_ref:
-#    zip_ref.extractall(__file__)
+#Descomprime el .zip para conseguir el .txt
+with zipfile.ZipFile(latest_file, 'r') as zip_ref:
+    zip_ref.extractall(patcito)
